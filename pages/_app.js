48c05fc63080e56060e458900cb7fddf2c868caf
agnivo988 <agnivamukherjee977@gmail.com>
2025-08-06 20:20:41 +0530

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
  
 export default function styles({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo.jpg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
